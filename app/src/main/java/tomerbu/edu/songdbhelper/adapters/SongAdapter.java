6878a6221f365ab87d1f17d2f26591c4d4ae9e92
javaashdod <java.ashdod@gmail.com>
2016-08-03 11:20:57 +0300

    Targil 2 intro

--- app/src/main/java/tomerbu/edu/songdbhelper/adapters/SongAdapter.java
+++ app/src/main/java/tomerbu/edu/songdbhelper/adapters/SongAdapter.java
@@ -8,6 +8,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,7 +16,7 @@ import tomerbu.edu.songdbhelper.R;
 import tomerbu.edu.songdbhelper.db.SongDAO;
 import tomerbu.edu.songdbhelper.models.Song;
 
-public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder>{
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder> {
 
     private final LayoutInflater inflater;
     private ArrayList<Song> songs;
@@ -23,7 +24,7 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
 
     public SongAdapter(Context context) {
         this.context = context;
-       requery();
+        requery();
         inflater = LayoutInflater.from(context);
     }
 
@@ -40,11 +41,21 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
 
     @Override
     public void onBindViewHolder(SongViewHolder holder, int position) {
-        Song s = songs.get(position);
+        final Song s = songs.get(position);
 
         holder.tvTitle.setText(s.getTitle());
         holder.tvArtist.setText(s.getArtist());
         holder.tvDuration.setText(s.getDuration());
+        holder._ID = s.getId();
+
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "Clicked " + s.getId(), Toast.LENGTH_SHORT).show();
+                //intent
+                //putExtra ID
+            }
+        });
     }
 
     @Override
@@ -52,8 +63,8 @@ public class SongAdapter extends RecyclerView.Adapter<SongAdapter.SongViewHolder
         return songs.size();
     }
 
-    public class SongViewHolder extends RecyclerView.ViewHolder{
-
+    public class SongViewHolder extends RecyclerView.ViewHolder {
+        String _ID;
         TextView tvTitle;
         TextView tvDuration;
         TextView tvArtist;
--- app/src/main/java/tomerbu/edu/songdbhelper/controllers/SongDetailsActivity.java
+++ app/src/main/java/tomerbu/edu/songdbhelper/controllers/SongDetailsActivity.java
@@ -23,6 +23,14 @@ public class SongDetailsActivity extends AppCompatActivity {
         etSongTitle = (EditText) findViewById(R.id.etTitle);
         etDuration = (EditText) findViewById(R.id.etDuration);
         etImageUri = (EditText) findViewById(R.id.etImage);
+
+        Intent intent = getIntent();
+        String id = intent.getStringExtra("_ID");
+        //if id is not null:
+        //Take the song by id from the database:
+        //AND Put the song detials in the editexts
+        //finally:
+        //The save method should call update instead of insert
     }
 
     public void save(View view) {
@@ -35,6 +43,8 @@ public class SongDetailsActivity extends AppCompatActivity {
                 etImageUri.getText().toString()
         );
 
+
+        //if (_id!=null) call update instead.
         dao.insert(s);
 
         Intent mainIntent = new Intent(this, SongDBActivity.class);
